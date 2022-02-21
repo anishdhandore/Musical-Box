a82e3ff1f82696459720a110fe766892f5191013
Anish <anish.dhandore@gmail.com>
2022-02-20 18:46:21 -0800

    final commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // javascript starts here
 
-var html_template = "<html><head><body><script>\n";
+var html_template = "<!DOCTYPE html><head><body><script>\n";
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function read_notes() {
+html_template += "function sleep(ms) { return new Promise((resolve) => setTimeout(resolve, ms));}"
+html_template += "async function play_notes() {"
+
+async function play_notes() {
   compiled_script = "";
   var notes = document.getElementById("notes").value.split("\n");
   i = 0;
@@ -14,47 +17,41 @@ async function read_notes() {
     j = 0;
     while (j != notes[i].length) {
       if (notes[i][j] == "A") {
-        console.log("Play A");
         new Audio("/tunes/A.wav").play();
         compiled_script += "new Audio('/tunes/A.wav').play();\n";
       }
       if (notes[i][j] == "B") {
-        console.log("Play B");
         new Audio("/tunes/B.wav").play();
         compiled_script += "new Audio('/tunes/B.wav').play();\n";
       }
       if (notes[i][j] == "C") {
-        console.log("Play C");
         new Audio("/tunes/C.wav").play();
         compiled_script += "new Audio('/tunes/C.wav').play();\n";
       }
       if (notes[i][j] == "D") {
-        console.log("Play D");
         new Audio("/tunes/D.wav").play();
         compiled_script += "new Audio('/tunes/D.wav').play();\n";
       }
       if (notes[i][j] == "E") {
-        console.log("Play E");
         new Audio('/tunes/E.wav').play()
         compiled_script += "new Audio('/tunes/E.wav').play();\n";
       }
       if (notes[i][j] == "F") {
-        console.log("Play F");
         new Audio('/tunes/F.wav').play()
         compiled_script += "new Audio('/tunes/F.wav').play();\n";
       }
       if (notes[i][j] == "G") {
-        console.log("Play G");
         new Audio('/tunes/G.wav').play()
         compiled_script += "new Audio('/tunes/G.wav').play();\n";
       }
       j++;
     }
     await sleep(1100);
+    compiled_script += "await sleep(1100);"
     i++;
   }
   document.getElementById("Compiled").value =
-    html_template + compiled_script + "</script></body></html>";
+    html_template + compiled_script + "}play_notes();</script></body></html>";
 }
 
 function clear_notes(){
